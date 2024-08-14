10547659f19cc4980caad44dc1001d745fce829f
Ulugbek <ulugbek9015y@gmail.com>
2024-08-14 23:44:43 +0500

    setup basicAuth

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,48 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { VersioningType } from '@nestjs/common';
 import { join } from 'path';
 import { existsSync, mkdirSync } from 'fs';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable API versioning
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  // Enable CORS
   app.enableCors();
 
+  // Set up Basic Auth for Swagger
+  app.use(
+    ['/api', '/api-json'], // These are the routes that Swagger uses
+    basicAuth({
+      users: { admin: '12345' }, // Set your desired username and password
+      challenge: true,
+    }),
+  );
+
+  // Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('The API description')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+
+  // Ensure the uploads directory exists
   const uploadDir = join(__dirname, '..', 'uploads');
   if (!existsSync(uploadDir)) {
     mkdirSync(uploadDir);
   }
+
+  // Set up Swagger
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Start the application
   await app.listen(3000);
 }
+
 bootstrap();
